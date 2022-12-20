1b19e84b7e077a3e5c99f855e72a6e8fdca86bc5
shella12 <ayeshaarshad4567@gmail.com>
2022-12-20 14:51:18 +0500

    Add navbar styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Dragons from './pages/Dragons';
+import Profile from './pages/Profile';
 import logo from './assets/planet-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt="logo-img" />
-      <h1>Space Travelers Hub</h1>
-      <nav>
-        <Link to="/">Rockets</Link>
-        <Link to="/Missions">Missions</Link>
-        <Link to="/Dragons">Dragons</Link>
-      </nav>
+      <header>
+        <div className="logo">
+          <img src={logo} alt="logo-img" />
+          <h1>Space Travelers Hub</h1>
+        </div>
+        <nav>
+          <Link to="/">Rockets</Link>
+          <Link to="/Missions">Missions</Link>
+          <Link to="/Dragons">Dragons</Link>
+          <Link to="/Profile">Profile</Link>
+        </nav>
+      </header>
       <Routes>
         <Route index element={<Rockets />} />
         <Route path="/Missions" element={<Missions />} />
         <Route path="/Dragons" element={<Dragons />} />
+        <Route path="/Dragons" element={<Profile />} />
       </Routes>
     </div>
   );
