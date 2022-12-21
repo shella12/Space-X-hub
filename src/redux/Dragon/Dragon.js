50c731c8860f94e10529f5701499d57b56da2d7b
Satyam Sharma <samwfelice@gmail.com>
2022-12-20 22:53:26 -0800

    Delete Initial State

--- src/redux/Dragon/Dragon.js
+++ src/redux/Dragon/Dragon.js
@@ -4,4 +4,3 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const fetchDragon = (payload) => ({ type: 'GET', payload });
 const url = 'https://api.spacexdata.com/v3/dragons';
 
-const initialState = [];
