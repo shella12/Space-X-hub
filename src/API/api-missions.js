9f412f19d5500d33367ff3e2a9e51a75853b9df4
shella12 <ayeshaarshad4567@gmail.com>
2022-12-21 12:42:34 +0500

    Add fetch mission action

---
+++ src/API/api-missions.js
@@ -0,0 +1,10 @@
+const url = 'https://api.spacexdata.com/v3/missions';
+
+const fetchMissions = async() => {
+    const response = fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+        throw (error);
+    });
+    return response;
+}
