07680b4e371f79cc7e2c545b3ea299bad66fd83e
Satyam Sharma <samwfelice@gmail.com>
2022-12-20 22:55:21 -0800

    Fetch data from dragons API using axios

--- src/redux/Dragon/Dragon.js
+++ src/redux/Dragon/Dragon.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const fetchDragon = (payload) => ({ type: 'GET', payload });
+const getDragon = (payload) => ({ type: 'GET', payload });
 const url = 'https://api.spacexdata.com/v3/dragons';
 
+export const fetchDragon = createAsyncThunk(
+    'books/fetchBooks',
+    async (_, { dispatch }) => {
+      const res = await axios.get(url);
+      const dragons = [];
+      dispatch(getDragon(dragons));
+    },
+  );
+  
