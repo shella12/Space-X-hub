d71182f64b6a45c70a9f52268324d0a100ba9e43
Satyam Sharma <samwfelice@gmail.com>
2022-12-21 02:38:36 -0800

    Rename ConfigStore

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import store from './redux/configureStore';
+import store from './redux/ConfigStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
