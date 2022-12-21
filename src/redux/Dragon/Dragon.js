a0ea44ae9e007b93e338c6adb7ab75d74b47be92
Satyam Sharma <samwfelice@gmail.com>
2022-12-20 23:02:38 -0800

    Fix linters

--- src/redux/Dragon/Dragon.js
+++ src/redux/Dragon/Dragon.js
@@ -4,10 +4,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const getDragons = (payload) => ({ type: 'GET', payload });
 const url = 'https://api.spacexdata.com/v3/dragons';
 
-export const fetchDragons = createAsyncThunk(
+const fetchDragons = createAsyncThunk(
   'dragon/fetchDragons',
   async (_, { dispatch }) => {
     const dragons = await axios.get(url);
-    dispatch(getDragon(dragons));
+    dispatch(getDragons(dragons));
   },
 );
+
+export default fetchDragons;
