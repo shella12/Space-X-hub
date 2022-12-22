d2dbf4875385e31c8c962233a0df80b6e5144be9
Satyam Sharma <samwfelice@gmail.com>
2022-12-22 07:00:29 -0800

    Create Table and render reserved dragons in profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,13 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
 const MyProfile = () => {
-  let dragons = useSelector((state) => state.dragons);
-  dragons = dragons.filter((dragon) => dragon.reserved);
+  const dragons = useSelector((state) => state.dragons);
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
 
   return (
-    <>
-    </>
+    <section className="container border-top rounded-0 border-2 d-flex justify-content-between gap-5">
+      <div className="col-6">
+        <h2>My Dragons</h2>
+        {!reservedDragons.length && <p>No Dragons Reserved</p>}
+        <Table bordered>
+          <tbody>
+            {reservedDragons.map((dragon) => (
+              <tr key={dragon.id}>
+                <td className="p-3">{dragon.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </section>
   );
 };
 
