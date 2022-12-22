0eda3e6934598159696e358afa8d1461f610abbd
Soe Sandar Win <soesandarwin2201@gmail.com>
2022-12-22 16:20:55 +0630

    solve the conflict between dragon branch and rocket branch

---
+++ src/pages/Dragons.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDragons } from '../redux/Dragon/Dragon';
+import DragonItem from '../components/DragonItem';
+
+const Dragons = () => {
+  const dragons = useSelector((state) => state.dragons);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDragons());
+  }, [dispatch]);
+
+  return (
+    <div className="list-container">
+      {dragons.map((dragon) => (
+        <DragonItem
+          key={dragon.id}
+          name={dragon.name}
+          type={dragon.type}
+          img={dragon.img}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Dragons;
--- src/redux/ConfigStore.js
+++ src/redux/ConfigStore.js
@@ -2,9 +2,13 @@ import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolk
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rocketReducer from './Rockets/RocketsSlice';
+import dragonsReducer from './Dragon/Dragon';
+import userSliceMissions from './Missions/Missions';
 
 const reducer = combineReducers({
   rockets: rocketReducer,
+  missionReducer: userSliceMissions,
+  dragons: dragonsReducer,
 });
 
 const store = configureStore({ reducer }, applyMiddleware(logger, thunk));
