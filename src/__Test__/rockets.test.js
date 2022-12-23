8996f7c69f97490e1f6a31806b153b6698858c21
Soe Sandar Win <soesandarwin2201@gmail.com>
2022-12-23 15:28:43 +0630

    Fix the linters

--- src/__Test__/rockets.test.js
+++ src/__Test__/rockets.test.js
@@ -1,25 +1,25 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import { render } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../redux/ConfigStore';
 import Rocket from '../pages/Rocket/Rocket';
 import RocketsContainer from '../pages/Rocket/Rockets';
 
 describe('testing Rocket component', () => {
- it('should render the rocket component', () => {
-  const rocket = render(
-   <Provider store={store}>
-   <Rocket />
-   </Provider>
-  );
-  expect(rocket).toMatchSnapshot();
- });
- it('should render the rockets component', () => {
-  const rockets = render(
-    <Provider store={store}>
-      <RocketsContainer />
-    </Provider>
-  );
-  expect(rockets).toMatchSnapshot();
- });
-})
+  it('should render the rocket component', () => {
+    const rocket = render(
+      <Provider store={store}>
+        <Rocket />
+      </Provider>,
+    );
+    expect(rocket).toMatchSnapshot();
+  });
+  it('should render the rockets component', () => {
+    const rockets = render(
+      <Provider store={store}>
+        <RocketsContainer />
+      </Provider>,
+    );
+    expect(rockets).toMatchSnapshot();
+  });
+});
