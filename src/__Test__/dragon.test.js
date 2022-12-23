cb6f771139ea84a1f7f1ce425c85a440d4188212
Soe Sandar Win <soesandarwin2201@gmail.com>
2022-12-23 19:16:35 +0630

    Make the test case for the dragon

---
+++ src/__Test__/dragon.test.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../redux/ConfigStore';
+import Dragons from '../pages/Dragons';
+
+describe('testing mission component', () => {
+  it('should render the mission component', () => {
+    const dragons = render(
+      <Provider store={store}>
+        <Dragons />
+      </Provider>
+    );
+    expect(dragons).toMatchSnapshot();
+  });
+});
